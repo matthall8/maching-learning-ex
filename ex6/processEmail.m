2ad0df7d3a183cd0dead1f7d26388f246530c1c8
Matt Hall <matthall8@gmail.com>
2020-05-10 17:56:08 +0100

    Finalised week 6

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -96,15 +96,11 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
-
-
-
-
-
-
-
+    for word = 1:length(vocabList)
+        if strcmp(str, vocabList(word))
+            word_indices = [word_indices ; word];
+        end
+    end
 
     % =============================================================
 
