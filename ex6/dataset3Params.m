0de4a122f2c0e0fba953b9a19eb91f2380fe5b36
Matt Hall <matthall8@gmail.com>
2020-05-10 16:07:29 +0100

    Part 2 complete

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -10,6 +10,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
+pos_vals = [0.01;0;03;0.1;0.3;1;3;10;30];
+c_sigma_error = [];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -24,9 +26,22 @@ sigma = 0.3;
 %
 
 
+for counter_c = 1:length(pos_vals)
+   for counter_sigma = 1:length(pos_vals)
+        temp_c = pos_vals(counter_c);
+        temp_sigma = pos_vals(counter_sigma);
+        model = svmTrain(X, y, temp_c, @(x1, x2) gaussianKernel(x1, x2, temp_sigma));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval)); 
+        c_sigma_error = [c_sigma_error;temp_c, temp_sigma, error];
+   end
+end
 
 
-
+[M,I] = min(c_sigma_error);
+min_location = I(3);
+C = c_sigma_error(min_location,1);
+sigma = c_sigma_error(min_location,2);
 
 
 % =========================================================================
